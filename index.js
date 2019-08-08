3d37f254086a7b4cf5f7fc00de1fa6e23699f0f2
cat12079801 <cat12079801@gmail.com>
2019-08-08 20:46:41 +0900

    🌟 react-navigationを使う

--- index.js
+++ index.js
@@ -2,10 +2,40 @@
  * @format
  */
 
+import React from 'react';
 import { AppRegistry } from 'react-native';
+import {
+  createAppContainer,
+  createStackNavigator,
+} from 'react-navigation';
+
 import App from './src/scenes/App';
 import { name as appName } from './app.json';
 
+const mainStackNavigator = createStackNavigator({
+  App: {
+    screen: App,
+    navigationOptions: {
+      gesturesEnabled: false,
+      header: null,
+    },
+  },
+}, {
+  initialRouteName: 'App',
+});
+
+const AppContainer = createAppContainer(mainStackNavigator);
+
+const AppContainerWrapper = () => {
+  return (
+    <AppContainer
+      // eslint-disable-next-line no-unused-vars
+      onNavigationStateChange={(prevState, currentState, action) => {
+      }}
+    />
+  );
+};
+
 AppRegistry.registerComponent(appName, () => {
-  return App;
+  return AppContainerWrapper;
 });
