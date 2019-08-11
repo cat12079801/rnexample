06c20f6ce33b713df96dcac9fa86776b7f995ab9
cat12079801 <cat12079801@gmail.com>
2019-08-11 17:09:32 +0900

    🌟 スライドページへ遷移するボタン配置

--- src/scenes/App.js
+++ src/scenes/App.js
@@ -12,11 +12,24 @@ import React, {
 } from 'react';
 import {
   Text,
+  TouchableOpacity,
   StyleSheet,
   View,
 } from 'react-native';
 
+const color = {
+  button: '#92f0d7',
+};
+
 const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: color.button,
+    borderRadius: 25,
+    height: 50,
+    justifyContent: 'center',
+    width: 150,
+  },
   container: {
     alignItems: 'center',
     flex: 1,
@@ -29,13 +42,24 @@ type PropsType = {
 };
 
 export default class App extends Component<PropsType> {
+  transition = (destination: string) => {
+    alert(destination);
+  }
+
   render(): React.DOM {
     return (
       <Fragment>
         <View style={styles.container}>
-          <Text>
-            hello world!
-          </Text>
+          <View>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => { this.transition('slide'); }}
+            >
+              <Text>
+                スライド
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </Fragment>
     );
