86f257d8e01d31ae20fac838904ebb9ecd15fc9e
cat12079801 <cat12079801@gmail.com>
2019-08-11 19:37:53 +0900

    🌟 スライドのページ仮組み

--- src/scenes/Slide.js
+++ src/scenes/Slide.js
@@ -8,31 +8,33 @@ import React, {
   Fragment,
 } from 'react';
 import {
+  Dimensions,
   Text,
   TouchableOpacity,
+  ScrollView,
   StyleSheet,
   View,
 } from 'react-native';
 
 import Scenes from 'rnexample/src/Scenes';
 
-const color = {
-  button: '#92f0d7',
-};
+const dimensions  = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  button: {
-    alignItems: 'center',
-    backgroundColor: color.button,
-    borderRadius: 25,
-    height: 50,
-    justifyContent: 'center',
-    width: 150,
-  },
   container: {
     alignItems: 'center',
     flex: 1,
     justifyContent: 'center',
+  },
+  scrollContent: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: dimensions.height,
+    width: dimensions.width,
+  },
+  scrollHorizontal: {
+  },
+  scrollVertical: {
   },
 });
 
@@ -45,11 +47,42 @@ export default class Slide extends Component<PropsType> {
     return (
       <Fragment>
         <View style={styles.container}>
-          <View>
-            <Text>
-              スライド予定
-            </Text>
-          </View>
+          <ScrollView
+            style={styles.scrollHorizontal}
+            horizontal={true}
+            pagingEnabled={true}
+          >
+            <ScrollView
+              style={styles.scrollVertical}
+              pagingEnabled={true}
+            >
+              <View style={styles.scrollContent}>
+                <Text>
+                  ページ1-1
+                </Text>
+              </View>
+              <View style={styles.scrollContent}>
+                <Text>
+                  ページ1-2
+                </Text>
+              </View>
+            </ScrollView>
+            <ScrollView
+              style={styles.scrollVertical}
+              pagingEnabled={true}
+            >
+              <View style={styles.scrollContent}>
+                <Text>
+                  ページ2-1
+                </Text>
+              </View>
+              <View style={styles.scrollContent}>
+                <Text>
+                  ページ2-2
+                </Text>
+              </View>
+            </ScrollView>
+          </ScrollView>
         </View>
       </Fragment>
     );
